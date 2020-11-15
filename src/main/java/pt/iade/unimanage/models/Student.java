703d7b425fb8030ecd58c8e58f474264866cb234
Tiago Vitor Nunes da Silva <tiaguinhoo956@gmail.com>
2020-11-15 13:33:08 +0000

    Git and classes

---
+++ src/main/java/pt/iade/unimanage/models/Student.java
@@ -0,0 +1,60 @@
+package pt.iade.unimanage.models;
+
+
+import java.time.LocalDate;
+
+
+        public class Student {
+            private static int nextNumber = 0;
+            private String name;
+            private LocalDate birthDate;
+            private String email;
+            private char gender;
+            private int number;
+ 
+ 
+            public Student(String name, LocalDate birthDate,char gender) {
+                this.name = name;
+                this.birthDate = birthDate;
+                this.gender = gender;
+                this.number = nextNumber;
+                nextNumber++;
+                email = "";
+                }
+ 
+                public String getName() { 
+                    return name;
+                 }
+                public void setName(String name) { 
+                    this.name = name; 
+                }
+                public LocalDate getBirthDate() {
+                 return birthDate;
+                    }
+                
+               
+
+                    public char getGender() {
+                        return gender;
+                    }
+                   
+                public void setGender(char gender) {
+                        this.gender = gender;
+                    }
+
+           
+                public String getEmail() {
+                     return email;
+                 }
+               
+                public void setEmail(String email) {
+                        this.email = email;
+                }
+            
+               
+                public int getNumber() {
+                        return number;
+                }
+        
+                                
+}
